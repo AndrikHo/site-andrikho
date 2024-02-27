9831fd747d8902620eb3d4b63bebb820c22e2d77
AndrikHo <161403317+AndrikHo@users.noreply.github.com>
2024-02-27 11:58:43 +0900

    Delete Site directory

--- Site/JS/app.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', e => {
-	document.body.style.cssText += `--scrolTop: ${this.scrollY}px`
-}) 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
-})
